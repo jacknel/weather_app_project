057a880e603d449a9130f7c9b1fb410f11b4c93e
jacknel <mjacknelson19@gmail.com>
2023-01-27 02:29:49 -0600

    fixed index.js

--- develop/assets/js/script.js
+++ develop/assets/js/script.js
@@ -1,101 +1,30 @@
-const apiKey = '7989e54da22b5217da0cf43c215b6b08'
-const apiLink = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid=7989e54da22b5217da0cf43c215b6b08'
-const cityInput = document.getElementById('search');
-const inputSubmit = document.getElementById('search-form');
+// const apiKey = '7989e54da22b5217da0cf43c215b6b08'
+// script.js
 
-const weatherData = (cityInput) => {
-    const URL = `api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=7989e54da22b5217da0cf43c215b6b08`
-    const oneDayForecast = fetch(URL)
-    oneDayForecast.then(response => {
-        return response.json();
-    })
-    .then(response => {
-        console.log(response.list.main.temp)
-    })
-}
+const apiKey = "7989e54da22b5217da0cf43c215b6b08";
 
-// const foodRecipe = (foodInput) => {
-//     const URL = `https://edamam-recipe-search.p.rapidapi.com/search?q=${foodInput}`
+// Wait for the DOM to be fully loaded
+$(document).ready(() => {
+    // Listen for the form submit event
+    $("#search-form").submit((event) => {
+        event.preventDefault();
 
-//     const optionsFood = {
-//         method: 'GET',
-//         headers: {
-//             'X-RapidAPI-Key': '3d36bbf348msh6d60b8e2f12876dp106ce1jsn2c433989fdc0',
-//             'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
-//         }
-//     };
-//     const recipeInfo = fetch(URL, optionsFood)
-//     recipeInfo.then(response => {
-//     return response.json();
-//     })
-//     // .then(response => {
-//     //     console.log(response.hits)
-//     //     const recipeArray = response.hits
-       
-//     //     for(let i=0; i<5; i++) {
-//     //         let recipe = recipeArray[i].recipe
-//     //         let cardColumn = document.getElementById('card-column')
+        // Get the search input value
+        const search = $("#search").val();
 
-//     //         let htmlString = `<div class="card light-green lighten-1">
-//     //         <div id="item-01">
-//     //         <div class="card-image recipeName"> 
-//     //         <img src="${recipe.image}"/>
-//     //         <span class="card-title"></span>
-//     //         </div>
-//     //         </div>
-//     //         <div id="card-info"  class="card-content white-text">
-//     //             <span class="card-title">${recipe.label}</span>
-//     //             <div class="divider"></div>
-             
-//     //             <div  id="url"><a href="${recipe.url}" target="_blank">Link to Recipe</a></div>
-                
-//     //             <div class="divider"></div>
-//     //             <h6>Nutriton</h6>
+        // Make an API call to OpenWeather
+        $.ajax({
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${apiKey}`,
+            method: "GET"
+        }).then((response) => {
+            // Update the forecast and search history elements in the HTML
+            $("#forecast").text(`The current weather in ${search} is ${response.weather[0].description} with a temperature of ${response.main.temp} degrees.`);
+            $("#search-history").append(`<p>${search}</p>`);
 
-//     //             <p id="calories">Calories: ${Math.round(recipe.calories)}</p>
-//     //             <p>Total Fats: ${Math.round(recipe.digest[0].total)}g</p>
-//     //             <p>Protien: ${Math.round(recipe.digest[2].total)}g</p>
-//     //             <p>Carbs: ${Math.round(recipe.digest[1].total)}g</p>
-                
-//     //             <div class="divider"></div>
-//     //             <div class="row" id="timerrow">
-//     //               <div class="col">
-//     //                 <i class="small material-icons">access_time</i>
-//     //               </div>
-//     //               <div class="col">
-//     //                 <p id="foodTime">Prep time ${recipe.totalTime} min</p>
-//     //               </div>
-                 
-                  
-                 
-//     //             </div>
-//     //             <div class="right-align">
-//     //             <button class="btn waves-effect waves-light saveRecipeBtn" type="submit" name="action">Save
-//     //                 <i class="material-icons right">save</i>
-//     //             </button>
-//     //             </div>
-//     //         </div>
-//     //     </div>`
-
-//     //         cardColumn.innerHTML += htmlString                    
-
-//     //     };
-        
-//     // })
-    
-//     // $(document).click(".saveRecipeBtn",function (event) {
-//     //     event.preventDefault();        
-//     //     var value = $(event.target).parent().siblings()[0].textContent
-//     //     var key = "SavedFoodRecipie"
-//     //         localStorage.setItem(key,value);        
-//     //  });
-// }
-
-
-
-inputSubmit.addEventListener("submit", function(event){
-    event.preventDefault()
-    event.stopPropagation()
-    console.log(cityInput.value)
-    weatherData(cityInput.value)
-})
+            //clear search input
+            $("#search").val("")
+        }).catch((error) => {
+            console.log(error);
+        });
+    });
+});
